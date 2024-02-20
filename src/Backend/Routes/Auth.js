6adc3fa601f0b68d06f0341e90258f8158ffb7b8
Elvis Sunguti <elviskendrick99@gmail.com>
2024-02-20 10:23:00 +0300

    Added BE code to log in user

--- src/Backend/Routes/Auth.js
+++ src/Backend/Routes/Auth.js
@@ -35,4 +35,34 @@ router.post("/signup", async (req, res) => {
     }
 });
 
+router.post("/login", async (req, res) => {
+    try{
+
+        // check if email exists
+        const user = await User.findOne({email: req.body.email});
+
+        // if user exist
+        if(user){
+            const passwordChecker = await bcrypt.compare(
+                req.body.passWord,
+                user.passWord
+            );
+
+            if(passwordChecker){
+                // generate a Jwt token for authentification
+                const token =await getToken(user.email, user)
+                return res.json({ message: "User logged in successfully" });
+            } else{
+                return res.json({ message: "user password does not match"});
+            }
+        } else{
+            return res.json({ message: "Email not found"});
+        }
+         
+    } catch(error){
+        console.error("Error signing in to the account:", error);
+        return res.json({ Error: "Error signing in to the account" });
+    }
+});
+
 module.exports = router;
